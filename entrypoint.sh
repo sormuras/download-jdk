cc18e1d1f55a2d454b90f55b80acf3d5440c5286
Christian Stein <sormuras@gmail.com>
2019-12-17 13:15:15 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+FEATURE=$1
 
-DOCKERFILE=${HOME}/jdk-14.dockerfile
-wget https://github.com/docker-library/openjdk/raw/master/14/jdk/Dockerfile --output-document ${DOCKERFILE}
+echo "Download JDK ${FEATURE}..."
+
+DOCKERFILE=${HOME}/jdk-$1.dockerfile
+wget https://github.com/docker-library/openjdk/raw/master/${FEATURE}/jdk/Dockerfile --output-document ${DOCKERFILE}
 
 JAVA_URL=$(cat ${DOCKERFILE} | sed -n 's/ENV JAVA_URL //p')
 JAVA_VERSION=$(cat ${DOCKERFILE} | sed -n 's/ENV JAVA_VERSION //p')
@@ -13,4 +15,4 @@ wget --directory-prefix ${HOME} ${JAVA_URL}
 echo ::set-env name=JAVA_VERSION::${JAVA_VERSION}
 echo ::set-env name=JDK_FILE::"${HOME}/$(basename ${JAVA_URL})"
         
-echo ::set-output name=java_version::${JAVA_VERSION}
+echo ::set-output name=jdk-file::"${HOME}/$(basename ${JAVA_URL})"
