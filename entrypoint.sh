a542967845889d1e967a4dda5cfa795ef1030d86
Christian Stein <sormuras@gmail.com>
2019-12-17 13:36:09 +0100

    😱

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,6 @@ JAVA_VERSION=$(cat ${DOCKERFILE} | sed -n 's/ENV JAVA_VERSION //p')
 wget --directory-prefix ${HOME} ${JAVA_URL}
 
 echo ::set-env name=JAVA_VERSION::${JAVA_VERSION}
-echo ::set-env name=JDK_FILE::"${HOME}/$(basename ${JAVA_URL})"
+echo ::set-env name=JDK_FILE::"/home/runner/work/_temp/_github_home/$(basename ${JAVA_URL})"
         
-echo ::set-output name=jdk-file::"${HOME}/$(basename ${JAVA_URL})"
+echo ::set-output name=jdk-file::"/home/runner/work/_temp/_github_home/$(basename ${JAVA_URL})"
