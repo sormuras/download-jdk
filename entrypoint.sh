2d17b531ab9bd4b7166996dddef7f5a947c3dabb
Christian Stein <sormuras@gmail.com>
2019-12-17 15:12:02 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,13 @@ DOCKERFILE=${HOME}/jdk-$1.dockerfile
 wget https://github.com/docker-library/openjdk/raw/master/${FEATURE}/jdk/Dockerfile --output-document ${DOCKERFILE}
 
 JAVA_URL=$(cat ${DOCKERFILE} | sed -n 's/ENV JAVA_URL //p')
-JAVA_VERSION=$(cat ${DOCKERFILE} | sed -n 's/ENV JAVA_VERSION //p')
+JDK_VERSION=$(cat ${DOCKERFILE} | sed -n 's/ENV JAVA_VERSION //p')
+JDK_FILE="/home/runner/work/_temp/_github_home/$(basename ${JAVA_URL})"
 
 wget --directory-prefix ${HOME} ${JAVA_URL}
 
-echo ::set-env name=JAVA_VERSION::${JAVA_VERSION}
-echo ::set-env name=JDK_FILE::"/home/runner/work/_temp/_github_home/$(basename ${JAVA_URL})"
+echo ::set-env name=JDK_FILE::${JDK_VERSION}
+echo ::set-env name=JDK_VERSION::${JAVA_VERSION}
         
-echo ::set-output name=jdk-file::"/home/runner/work/_temp/_github_home/$(basename ${JAVA_URL})"
+echo ::set-output name=file::${JDK_VERSION}
+echo ::set-output name=version::${JAVA_VERSION}
