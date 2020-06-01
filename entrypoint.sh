a79910b29b6add89a34d300ce1129cbdc9d0a600
Christian Stein <sormuras@gmail.com>
2020-06-01 14:19:44 +0200

    Back to install-jdk.sh again

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,16 @@ FEATURE=$1
 
 echo "Download JDK ${FEATURE}..."
 
-DOCKERFILE=${HOME}/jdk-$1.dockerfile
-wget https://github.com/docker-library/openjdk/raw/master/${FEATURE}/jdk/Dockerfile --output-document ${DOCKERFILE}
+wget https://github.com/sormuras/bach/raw/master/install-jdk.sh
+JAVA_URL=$(./install-jdk.sh --verbose --feature ${FEATURE} --dry-run --emit-url | tail --lines 1)
 
-JAVA_URL=$(cat ${DOCKERFILE} | sed -n 's/ENV JAVA_URL //p')
 JDK_FILE="/home/runner/work/_temp/_github_home/$(basename ${JAVA_URL})"
-JDK_VERSION=$(cat ${DOCKERFILE} | sed -n 's/ENV JAVA_VERSION //p')
+JDK_VERSION=${FEATURE}
 
 wget --directory-prefix ${HOME} ${JAVA_URL}
 
 echo ::set-env name=JDK_FILE::${JDK_FILE}
 echo ::set-env name=JDK_VERSION::${JDK_VERSION}
-        
+
 echo ::set-output name=file::${JDK_FILE}
 echo ::set-output name=version::${JDK_VERSION}
