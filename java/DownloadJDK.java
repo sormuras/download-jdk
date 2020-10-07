58087da63d95df5f59b92f6bac8f1420999bdab6
Christian Stein <sormuras@gmail.com>
2020-10-07 13:07:01 +0200

    Emit system properties and current JDK archives

--- java/DownloadJDK.java
+++ java/DownloadJDK.java
@@ -1,5 +1,19 @@
-class Action {
-  public static void main(String... args) {
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+class DownloadJDK {
+  public static void main(String... args) throws Exception {
     System.out.println("Download JDK");
+
+    System.getProperties().forEach((k, v) -> System.out.println(k + ":" + v));
+
+    var properties = browse("https://github.com/sormuras/bach/raw/HEAD/install-jdk.properties");
+    System.out.println(properties);
+  }
+
+  private static String browse(String url) throws Exception {
+    try (var stream = URI.create(url).toURL().openStream()) {
+      return new String(stream.readAllBytes(), StandardCharsets.UTF_8);
+    }
   }
 }
