c7aaa3e5548db47c9b45c8fc43ff9ac6df1c6ed3
Christian Stein <sormuras@gmail.com>
2020-10-08 10:54:05 +0200

    Dry-run and check outputs

--- java/DownloadJDK.java
+++ java/DownloadJDK.java
@@ -1,6 +1,11 @@
+import java.io.FileOutputStream;
 import java.io.StringReader;
 import java.net.URI;
+import java.net.URL;
+import java.nio.channels.Channels;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Properties;
 
 class DownloadJDK {
@@ -10,14 +15,16 @@ class DownloadJDK {
     var arch = computeArch();
 
     System.out.println("Download JDK " + feature + " (" + os + "-" + arch + ")");
-
     // System.getProperties().forEach((k, v) -> System.out.println(k + ":" + v));
 
     var properties = browse("https://github.com/sormuras/bach/raw/HEAD/install-jdk.properties");
     var pro = new Properties();
     pro.load(new StringReader(properties));
     var uri = pro.getProperty(feature + "-" + os + "-" + arch);
-    System.out.println(uri);
+
+    var path = download(uri);
+    Files.writeString(Path.of(".download-jdk.file"), path.toString());
+    Files.writeString(Path.of(".download-jdk.version"), feature);
   }
 
   private static String computeOs() {
@@ -35,5 +42,18 @@ class DownloadJDK {
     try (var stream = URI.create(url).toURL().openStream()) {
       return new String(stream.readAllBytes(), StandardCharsets.UTF_8);
     }
+  }
+
+  private static Path download(String uri) throws Exception {
+    var url = new URL(uri);
+    var file = uri.substring(uri.lastIndexOf('/') + 1);
+    var path = Files.createTempDirectory("download-jdk-").resolve(file);
+    System.out.println("  << " + url);
+    System.out.println("  >> " + path);
+    try (var source = Channels.newChannel(url.openStream());
+         var target = new FileOutputStream(path.toFile()).getChannel()) {
+      // target.transferFrom(source, 0, Long.MAX_VALUE);
+    }
+    return path;
   }
 }
